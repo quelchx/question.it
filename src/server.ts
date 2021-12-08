6f52336a83021a671540f7edd3a490f489d42ad3
quelchx <quelchx@gmail.com>
2021-12-08 18:29:53 -0500

    project init --tests passed

---
+++ src/server.ts
@@ -0,0 +1,40 @@
+import 'reflect-metadata'
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+import express from 'express'
+import morgan from 'morgan'
+import cookieParser from 'cookie-parser'
+
+import { createConnection } from 'typeorm'
+
+import authRoutes from './routes/auth'
+import postRoutes from './routes/posts'
+import subRoutes from './routes/subs'
+
+import trim from './middleware/trim'
+
+const app = express()
+const PORT = process.env.PORT
+
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(trim)
+app.use(cookieParser())
+
+app.get('/', (_, res) => res.send('Hello World'))
+app.use('/api/auth', authRoutes)
+app.use('/api/posts', postRoutes)
+app.use('/api/subs', subRoutes)
+
+app.listen(PORT, async () => {
+  console.log(`Server running at http://localhost:${PORT}`)
+
+  try {
+    await createConnection()
+    console.log('Database connected!')
+  } catch (err) {
+    console.log(err)
+  }
+})
